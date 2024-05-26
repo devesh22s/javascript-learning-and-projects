468ea013a98029332bbde4bc7a9e385bd8a4725e
devesh22s <139629713+devesh22s@users.noreply.github.com>
2024-05-26 20:15:12 +0530

    operations and comparisons

--- 04_operations.js
+++ 04_operations.js
@@ -1,22 +1,22 @@
 //   operations
 
-// let value = 5;
-// let negvalue = -value;
-// console.log(negvalue);
+let value = 5;
+let negvalue = -value;
+console.log(negvalue);
 
 
-// console.log(2+2);
-// console.log(2-2);
-// console.log(2*2);
-// console.log(2**2);
-// console.log(2/3);
-// console.log(2%3);
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2**2);
+console.log(2/3);
+console.log(2%3);
 
 
-// let str1 = "devesh ";
-// let str2 = "kumar";
-// let add = str1+str2;
-// console.log(add);
+let str1 = "devesh ";
+let str2 = "kumar";
+let add = str1+str2;
+console.log(add);
 
 
 console.log("2" +5);
@@ -27,3 +27,30 @@ let count = 100;
 console.log(count++);
 console.log(count);
 console.log(++count);
+
+
+
+// ------------------------------- comparisons -------------------------
+
+console.log(2>1);
+console.log(2>=1);
+console.log(2<1);
+console.log(2<=1);
+console.log(2==1);
+console.log(2 != 1);
+
+
+
+console.log(null >0);
+console.log(null == 0);
+console.log(null >= 0);
+
+
+console.log(undefined == 0);
+console.log(undefined >= 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+
+
+console.log("2" == 2);
