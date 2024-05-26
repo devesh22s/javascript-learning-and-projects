d0e517c120531dd626634c5e19465a887fe1a48a
devesh22s <139629713+devesh22s@users.noreply.github.com>
2024-05-26 20:05:33 +0530

    simple operations

---
+++ 04_operations.js
@@ -0,0 +1,29 @@
+//   operations
+
+// let value = 5;
+// let negvalue = -value;
+// console.log(negvalue);
+
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+
+// let str1 = "devesh ";
+// let str2 = "kumar";
+// let add = str1+str2;
+// console.log(add);
+
+
+console.log("2" +5);
+console.log("2" +5 +5);
+console.log(5 + 5 +"2");        // always use ()
+
+let count = 100;
+console.log(count++);
+console.log(count);
+console.log(++count);
