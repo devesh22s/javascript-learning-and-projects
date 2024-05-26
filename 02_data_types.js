f74be50363a8588695fa0c85636d5d043a651717
devesh22s <139629713+devesh22s@users.noreply.github.com>
2024-05-26 19:41:27 +0530

    data types in js

---
+++ 02_data_types.js
@@ -0,0 +1,10 @@
+//bigInt = manally use in trading , facebook etc for long data
+// string = " "
+// boolean = true/false
+//null --> standalone value
+// undefined --> 
+// symbol
+// object
+
+console.log(typeof "devesh");
+console.log(typeof null);
