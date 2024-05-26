6a03c635b0fb6b759d747193d71cb45d10b95934
devesh22s <139629713+devesh22s@users.noreply.github.com>
2024-05-26 20:26:07 +0530

    data types

--- 02_data_types.js
+++ 02_data_types.js
@@ -1,10 +1,38 @@
-//bigInt = manally use in trading , facebook etc for long data
+//  primitive data types --> 7 types
+
+
+// bigInt = manally use in trading , facebook etc for long data
 // string = " "
 // boolean = true/false
-//null --> standalone value
+// null --> standalone value
 // undefined --> 
 // symbol
-// object
+// number
 
 console.log(typeof "devesh");
 console.log(typeof null);
+
+const bignum = 123456788765432124567n
+console.log(typeof bignum);
+
+
+
+//  non primitive datatypes --> 3 types 
+
+//  array
+//  object
+//  functions
+
+
+
+const hero = [ "batman", "ironman", "thor"]
+let myobj = {
+    name: "devesh",
+    age: 20,
+    enrolledin : "software engineer"
+}
+
+
+const myfunction = function(){
+    console.log("hello world");
+}
