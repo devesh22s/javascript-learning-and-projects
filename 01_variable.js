cd78ec29473a189575beb21553f4894483a25d54
devesh22s <139629713+devesh22s@users.noreply.github.com>
2024-05-26 18:59:03 +0530

    declaration of variables in js

---
+++ 01_variable.js
@@ -0,0 +1,15 @@
+const name = "Devesh";
+let gmail ="devesh2262004@gmail.com";
+var city = "jaipur";
+
+// name = "jhon";   // not allowded
+gmail = "drtfcg@gmail.com";
+city = "kolkata";
+
+console.log(name);
+console.log(gmail);
+console.log(city);
+
+// prefer not to use var  because of issue in block scope and functional scope.
+
+console.table([name, gmail, city] );  // for seeing in table formate
