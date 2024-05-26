c2e4196387eb108e3317d9f5b05a5cfe07968def
devesh22s <139629713+devesh22s@users.noreply.github.com>
2024-05-26 19:52:59 +0530

    type conversion in js

---
+++ 03_type_conversion.js
@@ -0,0 +1,21 @@
+let marks = "55";
+console.log(typeof marks);
+
+let value = Number(marks);
+
+console.log(typeof value);
+console.log(value);
+
+// "33" --> converted into number
+// "35eyr" --> NaN - not a number
+
+
+let loggin = 1;
+let  convert = Boolean(loggin)
+console.log(convert);
+
+
+
+let log = "";
+let  convert2 = Boolean(log)
+console.log(convert2);
