033fb8c0f9b724a504ca3be4d2c67cd1c9754100
devesh22s <139629713+devesh22s@users.noreply.github.com>
2024-05-26 21:41:59 +0530

    maths in js

---
+++ 06_maths.js
@@ -0,0 +1,43 @@
+// const score = 100;
+// console.log(score);
+
+// const newsc = new Number(100);
+// console.log(newsc);
+// console.log(newsc.toString());
+// console.log(newsc.toFixed(3));
+
+
+
+
+// const another = 132.34
+// console.log(another.toPrecision(4));
+
+
+// const long = 100000000;
+// console.log(long.toLocaleString('en-IN'));
+// console.log(long.toLocaleString());
+
+
+
+
+//  math
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.9));
+// console.log(Math.ceil(4.9));
+// console.log(Math.floor(4.9));
+// console.log(Math.min(4,9));
+// console.log(Math.max(4,9));
+
+
+
+// console.log(Math.random());   // valuies always from 0 to 1
+// console.log(Math.floor(Math.random() *10) +1);   
+
+
+const min = 10;
+const max = 20;
+console.log(Math.random()* (max-min +1)+min);
+
+
